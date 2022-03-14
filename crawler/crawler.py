c6acd1768c44e9def5ef33c323d9ba98ffdefcb8
root <root@li1721-33.members.linode.com>
2022-03-14 05:43:39 +0000

    [fix] update chromedriver path

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -27,7 +27,7 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome = webdriver.Chrome('/root/tr-crawler/venv/crawler/chromedriver', options=chrome_options)
+chrome = webdriver.Chrome('/root/tr-tools/crawler/chromedriver', options=chrome_options)
 
 def check_cmp():
     try:
